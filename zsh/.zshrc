46fafdf68fbe2f83707195e0d5d9bdd9496f7d7f
felipe ospina <felipeospina21@hotmail.com>
2023-09-01 17:32:37 -0500

    update .zshrc

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,7 +4,11 @@ HISTSIZE=1000
 SAVEHIST=1000
 unsetopt autocd beep extendedglob notify
 bindkey -e
-zstyle :compinstall filename '/home/felipe/.zshrc'
+zstyle :compinstall filename "$HOME/.zshrc"
+
+# Version managers
+alias gvm="unalias gvm;  [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"; gvm $@"
+alias nvm="unalias nvm; [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"; nvm $@"
 
 source_if_exists () {
     if test -r "$1"; then
@@ -16,19 +20,17 @@ source_if_exists $DOTFILES/zsh/.bash_aliases
 source_if_exists $DOTFILES/zsh/load_nvmrc.zsh
 source_if_exists $DOTFILES/zsh/plugins.zsh
 
+# import ENV variables
+export PATH="$HOME:"$HOME/.local/bin":$PATH"
+
+eval "$(starship init zsh)"
+eval "$(atuin init zsh)"
+
 # Linux Homebrew
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
 
-# import ENV variables 
-export PATH="$HOME:"$HOME/.local/bin":$PATH"
 
-
-eval "$(starship init zsh)"
-
-[[ -s "/home/felipe/.gvm/scripts/gvm" ]] && source "/home/felipe/.gvm/scripts/gvm"
-if [ -e /home/felipe/.nix-profile/etc/profile.d/nix.sh ]; then . /home/felipe/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
-
-eval "$(atuin init zsh)"
+#if [ -e /home/felipe/.nix-profile/etc/profile.d/nix.sh ]; then . /home/felipe/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
--- zsh/load_nvmrc.zsh
+++ zsh/load_nvmrc.zsh
@@ -1,27 +1,52 @@
 # nvm autmoatic use of .nvmrc
+
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 autoload -U add-zsh-hook
 
-load-nvmrc() {
-  local nvmrc_path
-  nvmrc_path="$(nvm_find_nvmrc)"
+nvm_echo() {
+    command printf %s\\n "$*" 2>/dev/null
+}
 
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version
-    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+nvm_find_up() {
 
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
-      nvm use
+    local path_
+    path_="${PWD}"
+    while [ "${path_}" != "" ] && [ "${path_}" != '.' ] && [ ! -f "${path_}/${1-}" ]; do
+        path_=${path_%/*}
+    done
+    nvm_echo "${path_}"
+}
+
+nvm_find_nvmrc() {
+    local dir
+    dir="$(nvm_find_up '.nvmrc')"
+    if [ -e "${dir}/.nvmrc" ]; then
+        nvm_echo "${dir}/.nvmrc"
     fi
-  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
+}
+
+load-nvmrc() {
+    alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
+
+    if [[ -f "$PWD/package.json" ]]; then
+        local node_version="$(nvm version)"
+        local nvmrc_path="$(nvm_find_nvmrc)"
+
+        if [ -n "$nvmrc_path" ]; then
+            local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+            if [ "$nvmrc_node_version" = "N/A" ]; then
+                nvm install
+            elif [ "$nvmrc_node_version" != "$node_version" ]; then
+                nvm use
+            fi
+        elif [ "$node_version" != "$(nvm version default)" ]; then
+            echo "Reverting to nvm default version"
+            nvm use default
+        fi
+    fi
 }
 
 add-zsh-hook chpwd load-nvmrc

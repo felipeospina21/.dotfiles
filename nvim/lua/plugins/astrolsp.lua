e2d9b01c4d1bc27f8b8509719cfc268bc70ec9fb
felipe ospina <felipeospina21@hotmail.com>
2024-04-15 10:58:56 -0500

    fix eslint re-rendering issue

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -23,6 +23,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
+          -- "typescriptreact",
           -- "python",
         },
       },
@@ -30,6 +31,8 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
         -- "tsserver",
+        "eslint",
+        "typescript-tools",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function

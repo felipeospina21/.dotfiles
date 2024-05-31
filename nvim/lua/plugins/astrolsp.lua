ec04f5f32a1ab8b78dc908976d3620aff49686a1
felipe ospina <felipeospina21@hotmail.com>
2024-05-31 11:45:51 -0500

    ignore vtsls formatting

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -5,7 +5,7 @@
 return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
-  optional = true,
+  -- optional = true,
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
@@ -30,9 +30,10 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
-        -- "tsserver",
+        "tsserver",
         "eslint",
         "typescript-tools",
+        "vtsls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
--- nvim/lua/plugins/golang.lua
+++ nvim/lua/plugins/golang.lua
@@ -110,6 +110,7 @@ return {
   },
   {
     "ray-x/go.nvim",
+    enabled = false,
     dependencies = {
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",

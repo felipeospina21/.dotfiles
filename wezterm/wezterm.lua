2e6845b22f14ffabaddd4a8d3dcd0387676f5393
Felipe Ospina <felipeospina21@hotmail.com>
2026-08-05 11:16:20 -0500

    [wezterm]: update fallback font name to match latest version

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,8 +30,7 @@ local config = {
 	color_scheme = "astrodark",
 	-- color_scheme = "rose-pine",
 	-- color_scheme = "Solarized (dark) (terminal.sexy)",
-	-- TODO: Remove after wezterm is updated with latest Nerd Icon Font
-	font = wezterm.font_with_fallback({ "JetBrains Mono", "Symbols Nerd Font" }),
+	font = wezterm.font_with_fallback({ "JetBrains Mono", "Symbols Nerd Font Mono" }),
 	window_background_opacity = 0.85,
 	inactive_pane_hsb = { saturation = 0.8, brightness = 0.3 },
 	window_padding = { left = 4, right = 0, top = 2, bottom = 0 },
